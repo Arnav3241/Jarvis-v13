d4258fcd417cbe4adb8b3013699cf4e9925ddb8a
Avi Sinha <iamavisinha@gmail.com>
2024-07-11 15:46:38 +0530

    "Code Executer" task completed Updated gitignore to ignore venv Added requirements.txt

---
+++ Functions/Execute.py
@@ -0,0 +1,12 @@
+def ExecuteCode(code_str: str) -> None:
+    try:
+        exec(code_str)
+    except Exception as e:
+        print('Error in ExecuteCode function(Execute.py), code contained in input string has wrong syntax OR wrong datatype argument. Error:', e)
+
+
+# Test cases
+if __name__ == '__main__':
+    ExecuteCode('print("hello world")')
+    ExecuteCode('print("hi")\nprint("yo")')
+    ExecuteCode('import math\nprint(math.sqrt(49))')
