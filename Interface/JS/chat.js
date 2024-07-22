b0e4861cc7665688b69c601d9a044fddba3efae9
Arnav Singh <arnav.singh.legendary.202@gmail.com>
2024-07-22 16:32:43 +0530

    Added Chat History + Chat History Interface

--- Interface/JS/chat.js
+++ Interface/JS/chat.js
@@ -8,8 +8,8 @@ document.getElementById("chatButton").addEventListener("click", function(event){
   input = document.getElementById("msger-input").value;
   document.getElementById("msger-input").value = "";
   console.log(input);
-  // document.getElementById("msger-input").disabled = true;
-
+    // document.getElementById("msger-input").disabled = true;
+  if (input == "") { return; }
   var d = new Date();
 
   if (`${d.getMinutes}`.length == 1) { var min = `0${d.getMinutes()}`; } else { var min = d.getMinutes(); }
@@ -20,5 +20,36 @@ document.getElementById("chatButton").addEventListener("click", function(event){
   z.innerHTML = `<div class="msg-img"></div> <div class="msg-bubble"> <div class="msg-info"> <div class="msg-info-name"> Arnav Singh </div> <div class="msg-info-time">${hr}:${min}</div> </div> <div class="msg-text"> ${input} </div> </div>`;
 
   Chat.appendChild(z); 
+  Chat.scrollTop = Chat.scrollHeight;
+
+  eel.AddToUserHistory(input, d, "1", "user");
+});
+
+// Demo History
+// [{'Data': 'Hello', 'Date': '12/12/2021'}, {'Data': 'Hello', 'Date': '12/12/2021'}, {'Data': 'hello Howe are you', 'Date': '2024-07-21T09:36:29.001Z'}, {'Data': 'Yes it is working', 'Date': '2024-07-21T09:36:38.249Z'}, {'Data': 'Hello', 'Date': '2024-07-21T09:37:40.702Z'}, {'Data': 'Hello', 'Date': '2024-07-21T09:39:00.622Z'}]
+
+ChatHistory = []
+
+eel.RestoreHistory("1")((data) => {
+  ChatHistory = data;
+  console.log(data);
+  for (var i = 0; i < data.length; i++) {
+    var d = new Date(data[i]["Date"]);
+    if (`${d.getMinutes}`.length == 1) { var min = `0${d.getMinutes()}`; } else { var min = d.getMinutes(); }
+    if (`${d.getHours()}`.length == 1) { var hr = `0${d.getHours()}`; }    else { var hr = d.getHours(); }
+
+    if (data[i]["Role"] == "user") {
+      var z = document.createElement('div');
+      z.className = 'msg right-msg';
+      z.innerHTML = `<div class="msg-img"></div> <div class="msg-bubble"> <div class="msg-info"> <div class="msg-info-name"> Arnav Singh </div> <div class="msg-info-time">${hr}:${min}</div> </div> <div class="msg-text"> ${data[i]["Data"]} </div> </div>`;
+      Chat.appendChild(z);
+    }
+    else {
+      var z = document.createElement('div');
+      z.className = 'msg left-msg';
+      z.innerHTML = `<div class="msg-img"></div> <div class="msg-bubble"> <div class="msg-info"> <div class="msg-info-name"> Jarvis </div> <div class="msg-info-time">${hr}:${min}</div> </div> <div class="msg-text"> ${data[i]["Data"]} </div> </div>`;
+      Chat.appendChild(z);
+    }
+  }
   Chat.scrollTop = Chat.scrollHeight;
 });
--- main.py
+++ main.py
@@ -9,14 +9,44 @@
 #   try: print(a)
 #   except: SpeakSync("I'm sorry, I didn't get that.")
 
+import json
 import eel
 
 #? Some important inits
 eel.init("Interface")
 
+@eel.expose
+def AddToUserHistory(data, date, soul, role):
+  with open("Database/History/History.json", "r") as f:
+    history = json.load(f)
+    
+  with open("Database/History/History.json", "w") as f:
+    history[str(soul)]["history"].append({
+      "Data": data,
+      "Date": date,
+      "Role": role
+    })
+    json.dump(history, f, indent=2)    
+
+@eel.expose
+def RestoreHistory(soul):
+  print("Restoring history for ", soul)
+  
+  with open("Database/History/History.json", "r") as f:
+    history = json.load(f)
+    print(history)
+    return history[str(soul)]["history"]
+
+def on_close(page, sockets):
+	print(page, 'closed')
+	print('Still have sockets open to', sockets)
+
 if __name__ == '__main__': 
   try: 
-    eel.start("index.html", mode='chrome', size=(1500, 1200), position=(0, 0)) 
-  except: 
-    print("\n💀: Jarvis has encountered a fatal error. Please try later.")
+    eel.start("index.html", size=(1500, 1200), position=(0, 0), callback=on_close) 
+  except:
+    print(f"\n💀: Jarvis has encountered a fatal error. Please try later.")
     exit()
+
+# AddToUserHistory("Hello", "12/12/2021", "1")
+# print(RestoreHistory("1"))
